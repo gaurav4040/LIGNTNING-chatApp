fa82f0486e75844198b672678759c86d9112c5d1
gaurav4040 <jangragaurav593@gmail.com>
2025-08-29 21:28:45 +0530

    side bar online number

--- LIGHTNING-chatApp/FRONTEND/src/componentsUI/sideBar.tsx
+++ LIGHTNING-chatApp/FRONTEND/src/componentsUI/sideBar.tsx
@@ -40,7 +40,7 @@ const Sidebar = () => {
             />
             <span className="text-sm text-white">Show online only</span>
           </label>
-          <span className="text-xs text-zinc-500">({onlineUsers.length>0?onlineUsers.length -1:0} online)</span>
+          <span className="text-xs text-zinc-500">({onlineUsers.length>0?onlineUsers.length:0} online)</span>
         </div>
       </div>
 
